b55767ac0af0b2cc28b274c1dd7ac72dd6d3f689
Jean Lucas Barragan <113623052+JeanlucasBarragan@users.noreply.github.com>
2023-06-06 16:29:25 -0300

    compilei todos dados em uma única tabela

---
+++ dados.py
@@ -0,0 +1,12 @@
+import os  
+import pandas as pd
+
+lista_arquivo = os.listdir("C:\Users\Jean Lucas\Downloads\Vendas")  #importa 
+tabela_total = pd.DataFrame()  #cria tabela vazia 
+
+for arquivo in lista_arquivo:
+    if 'vendas' in arquivo.lower():
+        tabela = pd.read_csv(f'C:\Users\Jean Lucas\Downloads\Vendas/{arquivo}')
+        tabela_total = tabela_total.append(tabela)
+
+print(tabela_total)
--- desafio.py
+++
@@ -1,3 +0,0 @@
-import os
-lista_arquivo = os.listdir("C:\Users\Jean Lucas\Downloads\Vendas")
-print(lista_arquivo)
